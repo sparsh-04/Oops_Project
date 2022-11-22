3c96e63774254658276d35a33fec6f56573d7488
sparsh-04 <f20211320@hyderabad.bits-pilani.ac.in>
2022-11-22 12:27:29 +0530

    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*"],
+  content: [  './components/**/*.{html,js}',
+  './pages/**/*.{html,js}',
+  './index.html',
+  './src/**/*.js',
+  './node_modules/@my-company/tailwind-components/**/*.js',
+],
   theme: {
     extend: {
       width:{
