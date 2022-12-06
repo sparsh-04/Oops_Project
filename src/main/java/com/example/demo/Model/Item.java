ceaa61dd3e9a5e6e716d55f79136171f5d75e444
Rohan121002 <f20212739@hyderabad.bits-pilani.ac.in>
2022-12-06 21:39:17 +0530

    changed adminhome page userlist and add item of manager

--- src/main/java/com/example/demo/Model/Item.java
+++ src/main/java/com/example/demo/Model/Item.java
@@ -11,77 +11,96 @@ import javax.persistence.Id;
 @Entity
 public class Item {
   @Id
-  @GeneratedValue(strategy=GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Long code;
+
   public Long getCode() {
     return code;
   }
+
   public void setCode(Long code) {
     this.code = code;
   }
 
   private String name;
+
   public String getName() {
     return name;
   }
+
   public void setName(String name) {
     this.name = name;
   }
 
   private float price;
+
   public float getPrice() {
     return price;
   }
+
   public void setPhone(float price) {
     this.price = price;
   }
 
   @Column(name = "offer", nullable = true)
   private String offer;
+
   public String getOffer() {
     return offer;
   }
+
   public void setOffer(String offer) {
     this.offer = offer;
   }
 
   private int stock;
+
   public int getStock() {
     return stock;
   }
+
   public void setStock(int stock) {
     this.stock = stock;
   }
 
-  private int deliveryTime;
-  public int getDeliveryTime() {
-    return deliveryTime;
+  @Column(name = "description", nullable = true)
+  private String description;
+
+  public String getDescription() {
+    return description;
   }
-  public void setDeliveryTime(int deliveryTime) {
-    this.deliveryTime = deliveryTime;
+
+  public void setDescription(String offer) {
+    this.description = offer;
   }
 
   private boolean available;
+
   public boolean getAvailable() {
     return available;
   }
+
   public void setAvailable(boolean available) {
     this.available = available;
   }
 
   private String imageUrl;
+
   public String getImageUrl() {
     return imageUrl;
   }
+
   public void setImageUrl(String imageUrl) {
     this.imageUrl = imageUrl;
   }
 
   @Enumerated(EnumType.STRING)
   private ProductCatagory catagory;
+
   public ProductCatagory getCatagory() {
     return catagory;
   }
+
   public void setCatagory(ProductCatagory catagory) {
     this.catagory = catagory;
   }
