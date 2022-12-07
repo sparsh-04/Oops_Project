a15855486309f9de511fc798c42cff5d1b71f405
Aryan Gupta <f20210162@hyderabad.bits-pilani.ac.in>
2022-12-08 00:15:46 +0530

    ReplacedImages,madeUsersList,addedNavDestinations

--- src/main/java/com/example/demo/Controller/NavigationController.java
+++ src/main/java/com/example/demo/Controller/NavigationController.java
@@ -5,12 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.demo.DTO.ItemDTO;
 import com.example.demo.DTO.UserDTO;
 import com.example.demo.Model.Item;
+import com.example.demo.Model.User;
 import com.example.demo.Repository.ItemRepo;
+import com.example.demo.Repository.UserRepo;
 
 @Controller
 public class NavigationController {
@@ -18,6 +23,9 @@ public class NavigationController {
     private ItemRepo itemRepo;
 
     private List<Item> itemList;
+
+    @Autowired
+    private UserRepo userRepo;
 
     @GetMapping("")
     public String viewHomePage(Model model){
@@ -53,7 +61,7 @@ public class NavigationController {
         return "Manager_Homepage";
     }
 
-    @GetMapping(value = "/Add-Item")
+    @GetMapping(value = "Manager/Add-Item")
     public String goToAddItemPage(Model model){
         model.addAttribute("item", new ItemDTO());
         return "Manager/Additem";
@@ -62,11 +70,40 @@ public class NavigationController {
     @GetMapping(value = "/Admin/Add-User")
     public String goToAddUserPage(Model model){
         model.addAttribute("user" , new UserDTO());
-        return "Admin/AddUser";
+        return "adduser";
     }
 
     @GetMapping(value = "/Customer-Support")
     public String goToCustomerSupport(Model model){
         return "Customerservice";
     }
+    
+  @GetMapping("/Admin/Users")
+  public String showUsersList(Model model){
+    List<User> users = userRepo.findAll();
+    model.addAttribute("users", users);
+    return "Userlist";
+  }
+  
+  @GetMapping("/Manager/Item/{ItemId}")
+  public String showItemDesc(Model model, @PathVariable(value="itemId") long id){
+    User user = userRepo.findById(id).orElse(null);
+    if(user == null){
+        return "userinfo?noexist";
+    }
+    model.addAttribute("user", user);
+
+    return "userinfo";
+  }
+
+  @GetMapping("/Admin/User/{UserId}")
+  public String showDesc(Model model, @PathVariable(value="itemId") long id){
+    Item item = itemRepo.findById(id).orElse(null);
+    if(item == null){
+        return "iteminfo?noexist";
+    }
+    model.addAttribute("item", item);
+
+    return "iteminfo";
+  }
 }
--- src/main/java/com/example/demo/Controller/UserController.java
+++ src/main/java/com/example/demo/Controller/UserController.java
@@ -1,10 +1,13 @@
 package com.example.demo.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.example.demo.DTO.UserDTO;
