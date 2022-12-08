011146a29cf4cbd42b697e2fbb5f5ef301f39b32
kshitizag <f20211818@hyderabad.bits-pilani.ac.in>
2022-12-08 16:35:25 +0530

    chngjsjgs

---
+++ src/main/java/com/example/demo/chart/CanvasjsChartController.java
@@ -0,0 +1,26 @@
+package com.example.demo.chart;
+ 
+import java.util.List;
+import java.util.Map;
+ 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+ 
+@Controller
+@RequestMapping("/canvasjschart")
+public class CanvasjsChartController {
+ 
+	@Autowired
+	private canvasjschartservice canvasjsChartService;
+ 
+	@RequestMapping(method = RequestMethod.GET)
+	public String springMVC(ModelMap modelMap) {
+		List<List<Map<Object, Object>>> canvasjsDataList = canvasjsChartService.getCanvasjsChartData();
+		modelMap.addAttribute("dataPointsList", canvasjsDataList);
+		return "chart";
+	}
+ 
+}       
